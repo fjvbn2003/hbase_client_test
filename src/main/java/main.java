3d7002599437a4bdec7b798cc513824cecc4c837
Youngju KIM <fjvbn2003@gmail.com>
2022-12-18 10:29:08 +0900

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class main {
     public static void main(String[] args) throws IOException {
-        HitBaseAdmin hitbase_admin = new HitBaseAdmin("apache-hbase001-test-cdh2-jp2v-dev.lineinfra-dev.com,apache-hbase002-test-cdh2-jp2v-dev.lineinfra-dev.com,apache-hbase003-test-cdh2-jp2v-dev.lineinfra-dev.com", "2181");
+        HitBaseAdmin hitbase_admin = new HitBaseAdmin("ubuntu01,ubuntu02,ubutnu03", "2181");
         HBaseAdmin admin = hitbase_admin.getAdmin();
         System.out.println(admin.getMaster());
     }
